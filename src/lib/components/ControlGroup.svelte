0d6ae9500f845c683ad5d19055559fbd74be634a
Jack Weilage <jack@weilage.dev>
2023-10-18 10:08:00 -0700

    Update dependencies

---
+++ src/lib/components/ControlGroup.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Map } from 'mapbox-gl'
+
+	import { setContext } from 'svelte'
+
+	export let position: Parameters<Map['addControl']>[1]
+
+	setContext('mapbox-control-position', position)
+</script>
+
+<slot />
