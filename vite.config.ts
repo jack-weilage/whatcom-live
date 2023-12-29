5faa8701fc509183aba5bd9825c89195163e3fd3
Jack Weilage <jack@weilage.dev>
2023-12-28 21:47:26 -0800

    chore: Remove unused `@ts-expect-error` comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
-//@ts-expect-error - Vite problems?
 export default defineConfig({
-	//@ts-expect-error - Vite problems?
 	plugins: [sveltekit()],
 })
