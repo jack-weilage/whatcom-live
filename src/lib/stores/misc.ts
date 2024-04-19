91a250811c9475b49f8624c0df5389d318013955
Jack Weilage <jack@weilage.dev>
2024-04-19 11:50:44 -0700

    feat: Create derived layer store

--- src/lib/stores/misc.ts
+++ src/lib/stores/misc.ts
@@ -1,13 +1,37 @@
+// BUG: Unsure where, but somehow the routes don't get populated until vehicles are fetched for a second time
+// This is pretty annoying, but only happens occasionally
+import { browser } from '$app/environment'
+import type { Writable } from 'svelte/store'
 import { get, readable, writable } from 'svelte/store'
 
 export function createLayerStore<T>(initial_value: T, initial_visibility: boolean, url: string) {
-	const { update, subscribe } = writable({
-		data: initial_value,
-		visible: initial_visibility,
-		loaded: false,
-	})
+	const data_store = writable(initial_value)
+	const { update, subscribe } = writable(
+		{
+			data: initial_value,
+			visible: initial_visibility,
+			loaded: false,
+		},
+		(_, update) => {
+			// Don't update unless the layer is visible.
+			if (!get({ subscribe }).visible || !browser) {
+				return
+			}
+
+			fetch(url)
+				.then((res) => res.json() as Promise<T>)
+				.then((data) => {
+					data_store.set(data)
+					update((store) => ({ ...store, loaded: true }))
+				})
+
+			return () => {}
+		},
+	)
+	data_store.subscribe((data) => update((store) => ({ ...store, data })))
 
 	return {
+		data_store,
 		fetch(fn: (data: T) => T = (data) => data) {
 			// Don't update unless the layer is visible.
 			if (!get({ subscribe }).visible) {
@@ -16,14 +40,39 @@ export function createLayerStore<T>(initial_value: T, initial_visibility: boolea
 
 			return fetch(url)
 				.then((res) => res.json() as Promise<T>)
-				.then((data) => update((store) => ({ ...store, data: fn(data), loaded: true })))
+				.then((data) => {
+					data_store.set(fn(data))
+					update((store) => ({ ...store, loaded: true }))
+				})
 		},
-		async show() {
+		show() {
 			update((store) => ({ ...store, visible: true }))
 
 			if (!get({ subscribe }).loaded) {
-				await this.fetch()
+				void this.fetch()
 			}
+		},
+		hide() {
+			update((store) => ({ ...store, visible: false }))
+		},
+		subscribe,
+	}
+}
+export function createDerivedLayerStore<T, U>(
+	derived_store: Writable<U>,
+	deriver: (v: U) => T,
+	initial_visibility: boolean,
+) {
+	const { update, subscribe } = writable({
+		data: deriver(get(derived_store)),
+		visible: initial_visibility,
+	})
+
+	derived_store.subscribe((data) => update((store) => ({ ...store, data: deriver(data) })))
+
+	return {
+		show() {
+			update((store) => ({ ...store, visible: true }))
 		},
 		hide() {
 			update((store) => ({ ...store, visible: false }))
