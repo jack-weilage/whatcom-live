5f709566bf8b3d530225f32015e64bd6a55584f7
Jack Weilage <jack@weilage.dev>
2024-04-19 11:57:50 -0700

    fix: Format code

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -40,8 +40,8 @@ type Events = ButtonPrimitive.Events
 export {
 	//
 	Root as Button,
-	buttonVariants,
 	Root,
+	buttonVariants,
 	type Events as ButtonEvents,
 	type Props as ButtonProps,
 	type Events,
--- src/lib/components/ui/tooltip/tooltip-content.svelte
+++ src/lib/components/ui/tooltip/tooltip-content.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { Tooltip as TooltipPrimitive } from "bits-ui";
-	import { cn, flyAndScale } from "$lib/shadcn-utils.js";
+	import { Tooltip as TooltipPrimitive } from 'bits-ui'
+	import { cn, flyAndScale } from '$lib/shadcn-utils.js'
 
-	type $$Props = TooltipPrimitive.ContentProps;
+	type $$Props = TooltipPrimitive.ContentProps
 
-	let className: $$Props["class"] = undefined;
-	export let sideOffset: $$Props["sideOffset"] = 4;
-	export let transition: $$Props["transition"] = flyAndScale;
-	export let transitionConfig: $$Props["transitionConfig"] = {
+	let className: $$Props['class'] = undefined
+	export let sideOffset: $$Props['sideOffset'] = 4
+	export let transition: $$Props['transition'] = flyAndScale
+	export let transitionConfig: $$Props['transitionConfig'] = {
 		y: 8,
 		duration: 150,
-	};
-	export { className as class };
+	}
+	export { className as class }
 </script>
 
 <TooltipPrimitive.Content
@@ -19,8 +19,8 @@
 	{transitionConfig}
 	{sideOffset}
 	class={cn(
-		"z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground",
-		className
+		'z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground',
+		className,
 	)}
 	{...$$restProps}
 >
